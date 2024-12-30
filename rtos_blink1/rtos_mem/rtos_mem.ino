a78810091ce00a342604730906a36b66dcba3a1f
diolapupo <paragun11@gmail.com>
2024-12-30 12:11:26 +0100

    completed the stack overflow code

--- rtos_blink1/rtos_mem/rtos_mem.ino
+++ rtos_blink1/rtos_mem/rtos_mem.ino
@@ -4,9 +4,26 @@ static const BaseType_t app_cpu = 0;
 static const BaseType_t app_cpu = 1;
 #endif
 
-void setup() {
-  // put your setup code here, to run once:
+void testTask(void *parameter){
+  while(1){
+    int a = 1;
+    int b[100];
 
+    for(int i = 0; i < 100; i++){
+      b[i] = a + 1;
+      }
+      Serial.println(b[0]);
+    }
+  }
+void setup() {
+  Serial.begin(115200);
+
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  Serial.println();
+  Serial.println("--FreeRTOS Memory Demo ---");
+
+  xTaskCreatePinnedToCore(testTask, "Test Task", 1024, NULL, 1, NULL, app_cpu);
+  vTaskDelete(NULL);
 }
 
 void loop() {
